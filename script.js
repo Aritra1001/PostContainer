1002e0b4a015243e947f5fbae39cdb4da9a9d024
Aritra Nandi <aritranandi30@gmail.com>
2025-11-14 19:31:41 +0530

    basic setup files

---
+++ script.js
@@ -0,0 +1,9 @@
+let post1 = {
+  id: 1,
+  author: 'John',
+  content: 'My first Post!',
+  likes: 10,
+  comments: ['Great post!', 'Nice photo!'],
+  image: 'https://files.codingninjas.in/image2-28694.jpg'
+};
+
